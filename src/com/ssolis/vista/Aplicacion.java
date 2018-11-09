9bec3162fca0870ba99703f3c11785411a757f8c
Santiago <sys.solis@gmail.com>
2018-11-09 12:11:36 -0500

    Proyecto coordenadas geometricas APLICACION

---
+++ src/com/ssolis/vista/Aplicacion.java
@@ -0,0 +1,22 @@
+package com.ssolis.vista;
+
+import com.ssolis.controlador.Coordinador;
+
+/**
+  * @Instituto Rumiñahui
+ * @author SSolis
+ */
+public class Aplicacion {
+    public static void main(String[] args) {
+        Coordinador c= new Coordinador();
+        c.menu();
+    }
+
+}
+
+
+
+
+
+
+
